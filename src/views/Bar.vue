80f2e19558457bfb6ddd5af2f798af8f69eed230
Matěj <matejkopecky@cermitech.com>
2021-09-30 12:02:50 +0200

    git upload

---
+++ src/views/Bar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>Bar Chart Example in Vue</h3>
+    <bar-chart :styles="myStyles" />
+    <button @click="increase()">Increase height</button>
+  </div>
+</template>
+
+<script>
+import BarChart from '@/components/BarChart'
+
+export default {
+  components: {
+    BarChart
+  },
+  data () {
+    return {
+      height: 300,
+      width: 300
+    }
+  },
+  methods: {
+    increase () {
+     this.height += 30
+    }
+  },
+  computed: {
+    myStyles () {
+      return {
+        height: `${this.height}px`,
+        width: `${this.width}px`,
+        position: 'absolute'
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/Multiple.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Multiple graphs</h3>
+    <bar-chart :width="300" :height="300" />
+    <line-chart :width="1200" :height="350" />
+    
+  </div>
+</template>
+
+<script>
+import BarChart from '@/components/BarChart'
+import LineChart from '@/components/LineChart'
+
+export default {
+  components: {
+    BarChart,
+    LineChart
+  }
+}
+</script>
